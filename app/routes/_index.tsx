c95507e3255afde7811153fb95709e528985aa75
Philipp <phidadev@gmail.com>
2024-12-22 12:06:09 +0100

    Produkt Loader hinzugefügt

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,26 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { getProducts } from "../products";
+import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({
     params,
     request
 }: LoaderFunctionArgs) => {
+    const url = new URL(request.url);
+    const page = url.searchParams.get("page") ?? 1;
+    const itemsPerPage = 6;
+    const products = await getProducts(Number(page), itemsPerPage);
 
-    return null;
+    return json({products: products});
 }
 
 export default function Index() {
+    const loaderData = useLoaderData<typeof loader>();
+    const products = loaderData.products ?? [];
+    
     return (
         <div>
-            
+
         </div>
     );
 }
